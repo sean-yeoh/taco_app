11b51c03f3343bc3dd7c49cf72c0432bde4d05d4
Sean Yeoh <sean.yeoh@hotmail.com>
2019-05-30 01:07:50 +0800

    add taco json. fix taco index api call

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,4 +1,29 @@
 class TacosController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create, :delete]
+  before_action :set_taco, only: [:show, :delete]
   def index
+    @tacos = Taco.all
+  end
+
+  def create
+    @taco = Taco.new(taco_params)
+    if @taco.save
+      render json: @taco
+    else
+      render json: @taco.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @taco.destroy
+  end
+
+  private
+  def taco_params
+    params.require(:taco).permit(:meat, :rice, :salsa, :notes)
+  end
+
+  def set_taco
+    @taco = Taco.find(params[:id])
   end
 end
---
+++ app/views/tacos/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.array! @tacos do |taco|
+  json.id taco.id
+  json.meat taco.meat
+  json.rice taco.rice
+  json.salsa taco.salsa
+  json.notes taco.notes
+end
